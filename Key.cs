f117b82e0a15b26e09fa7471e33f6f0e28170e5b
Kellenes <123210242+Kellenes@users.noreply.github.com>
2023-01-21 10:12:37 +0200

    Add files via upload

---
+++ Key.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    public GameObject key;
+    public GameObject door;
+    private bool Entered = false;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            Entered = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            Entered = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (Entered)
+        {
+            Destroy(key);
+            Destroy(door);
+        }
+    }
+}
