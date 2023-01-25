f8ea8867022adfa3ade6af0a329c535ec91ccd3e
Kellenes <123210242+Kellenes@users.noreply.github.com>
2023-01-25 19:54:18 +0200

    Add files via upload

---
+++ slow.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class slow : MonoBehaviour
+{
+    private void slow()
+    {
+        int i = 0;
+        if (Input.GetKeyDown(KeyCode.E))
+        {   
+            Time.timeScale = 0.5F;
+            movementSpeed = movementSpeed*2;
+            while(i < 10)
+            {
+                i++;
+            }
+            Time.timeScale = 1;
+            movementSpeed = movementSpeed/2;
+        } 
+        //Time.timeScale = 1;
+        //movementSpeed = movementSpeed/2;
+    }
+
+}
+//
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if(Time.timeScale == 0.5F)
+            {
+                Time.timeScale = 1;
+                movementSpeed = movementSpeed/2;
+            }
+
+            else if(Time.timeScale == 1)
+            {
+                Time.timeScale = 0.5F;
+                movementSpeed = movementSpeed*2;
+            }
+        }   
+    }
+//
+//
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Time.timeScale = 0.5F;
+            movementSpeed = movementSpeed*2;
+            Thread.Sleep(5000);   //остонавливает абсолютно всё
+            Time.timeScale = 1;
+            movementSpeed = movementSpeed/2;         
+        }
+    }
+//
+//
+    private void slow()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {   
+            Time.timeScale = 0.5F;
+            movementSpeed = movementSpeed*2;
+            Invoke(nameof(time), 3); //Waits for 3 seconds before calling "time"
+        }
+    }
+
+    private void time()
+    {
+        Time.timeScale = 1;
+        movementSpeed = movementSpeed/2;
+    }
+//
